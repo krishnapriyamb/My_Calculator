9f3f05fe6f74b7d36fe1b9fdfa49ef26f54bb1b8
krishnapriyamb <krishnapriya7251@gmail.com>
2023-04-20 16:40:51 +0530

    initial

---
+++ calculator.js
@@ -0,0 +1,21 @@
+
+// function clearScreen(){
+//     document.getElementsByClassName("btn btn-warning").value=" "
+// }
+
+// function calculate(event){
+//     let btn=event.target.name;
+//     let n1=document.querySelector("#id_num1").value
+//     let n2=document.querySelector("#id_num2").value
+//     let res=0;
+//     if(btn=="btn-add"){
+//         res=Number(n1)+Number(n2)
+//     }
+//     if(btn=="btn-sub"){
+//         res=Number(n1)-Number(n2)
+//     }
+//     document.querySelector("#id_result").innerHTML=`
+//     <div class="alert alert-primary" role="alert">
+//   Result=${res}
+// </div>`
+// }
